3b9cfb03ce17607596d5948b4841dac20f7135b3
Laurie Amen <lauriea@nia-login01.scinet.local>
2024-10-01 17:29:07 -0400

    Saving changes to kriging.py

--- kriging.py
+++ kriging.py
@@ -6,6 +6,7 @@ import numpy as np
 import scipy
 from matplotlib import pyplot as plt
 import copy
+from scipy.signal import find_peaks
 
 # Custom modules
 import data
@@ -34,8 +35,8 @@ except:
 
 ''' Frequency range ------------------------------------------'''
 freqarr, freqstep = np.linspace(0,250,4096,retstep=True) # Based on number of frequency channels of the antenna
-minfreq = 70
-maxfreq = 90
+minfreq = 70 # for testing
+maxfreq = 90 # for testing
 minfreqarg = int(minfreq/freqstep)
 maxfreqarg = int(maxfreq/freqstep)
 '''-------------------------------------------------------------'''
@@ -52,6 +53,15 @@ def ACF_fit(a,b,functype='linear'):
         return lambda dt: a*dt+b
     elif functype == 'exponential':
         return lambda dt: a*np.exp(-dt/b)
+    
+def delta_function(x, location=0, amplitude=0, width=1):
+    delta = np.zeros_like(x)
+    # Add a sharp peak at the specified location
+    idx = np.argmin(np.abs(x - location))  # Find the index closest to the location
+    if x[idx] == location:
+        # we only want the peak at exactly zero
+        delta[idx:idx+width] = amplitude  # Set the value to amplitude (height of delta peak)
+    return delta
 '''-------------------------------------------------------------'''
 
 
@@ -73,13 +83,13 @@ class Kriging:
             self.interp_times = self.interp_times[tmask_ant]
     
     
-    def __call__(self,dt,tmax=2*86400,dtmax=2*86400,acf_functype='linear'):
+    def __call__(self,dt=5,tmax=2*86400,dtmax=2*86400,acf_functype='linear'):
         '''
         When called, the class computes the ACF, and computes the interpolation at every interp_time.
         
         Parameters
         -----------
-        dt: timescale step for computation of the ACF.
+        dt: timescale step for computation of the ACF. Default 5 seconds (<typical smallest timegap between measurements(6-7s)).
         tmax: Maximum timescale for calculation of ACF. In seconds. Default 2x86400 seconds (48 hours).
         dtmax: Maximum (cutoff) dt of the ACF used for Kriging. Default 2x86400 seconds (48 hours).
         acf_functype: 'linear' or 'exponential', default 'linear'. Defines what model will be used to fit the ACF.
@@ -89,13 +99,17 @@ class Kriging:
         
         # We have to do the interpolation one frequency channel at a time
         for i,freq in enumerate(freqarr[minfreqarg:maxfreqarg]):
-            if i == 1: break # for testing
+            if i == 1: break # for testing, we break after 1 freq channel
+            
             # Compute the ACF from data
             freq_index = int(freq/freqstep)
             tot,wt = self.make_acf(self.data[:,freq_index]-self.data[:,freq_index].mean(),self.time,dt,tmax)
             mm=wt>30 # mask ACF entries with insignificant weights?
             self.acf_tvec = np.arange(len(tot))[mm]*dt
             self.acf = tot[mm]/wt[mm]
+            
+            # Smooth the ACF, overwrite ACF values/times with smoothed values
+            self.acf_tvec, self.acf = self.smooth_acf(tvec=self.acf_tvec,acf=self.acf)
             
             # Fit the ACF from dt=0 to dt=dtmax
             self.acf_func = self.fit_acf(tvec=self.acf_tvec,acf=self.acf,dtmax=dtmax,functype=acf_functype)
@@ -104,13 +118,12 @@ class Kriging:
             for j, tt_interp in enumerate(self.interp_times):
                 
                 # Compute the covariance matrix for the current antenna time, and perform interpolation
-                self.CMatrix, self.interp_data[i,j],self.interp_std[i,j] = self.compute_covariance_and_krig(dat=self.data[:,freq_index],t=self.time,
-                                                       dtmax=dtmax,interp_time=tt_interp,ACF_func=self.acf_func)
+                self.CMatrix, self.interp_data[i,j],self.interp_std[i,j] = self.compute_covariance_and_krig(dat=self.data[:,freq_index],t=self.time, dtmax=dtmax,interp_time=tt_interp,ACF_func=self.acf_func)
                 
     
         
     def make_acf(self,dat,t,dt,tmax):
-        '''Function to compute the autocorrelation function of given data. Provided by Jon Sievers.
+        '''Function to compute the autocorrelation function of given data. Provided by Jon Sievers. Includes dt=0 bin.
         
         Parameters
         -----------
@@ -136,26 +149,86 @@ class Kriging:
         return tot,wt
     
     
-    def fit_acf(self,tvec,acf,dtmax,functype='linear'):
+    def smooth_acf(self,tvec,acf,cycle_jump=60):
+        '''Function to smooth the ACF data by averaging ACF values obtained from calibrator data taken in a single calibrator measurement cycle, while conserving the dt=0 peak due to measurement noise. This translates to averaging data taken in close succession (~a few seconds time gap). Larger timegaps (e.g. order 0.5-1h) separate measurement cycles. We look for large peaks in the timegaps between subsequent data to separate into measurement cycles.
+        
+        Parameters
+        -----------
+        cycle_jump: Minimum timegape between subsequent measurements to separate them into two different measurement cycles, in seconds. Default 60 seconds.
+        
+        
+        '''
+        # Separate the current dt values into bins within a few minutes of each other, I think this normally corresponds to data taken during the same rotation through calibrators before going back to antenna
+        
+        time_values = tvec[1:] # we only smooth for dt>0
+        acf_values = acf[1:] # "     "      "
+        zerobin = acf[0] # save the zero-bin
+        tsteps = np.diff(time_values)
+
+        # Create bin edges based on big jumps in the timestep between subsequent measurements
+        bins = find_peaks(tsteps,height=cycle_jump)[0] # bin edges, minimum jump for separate cycle set to 60 by default
+
+        # Split data into the bins
+        bin_tgroups = np.split(time_values,bins+1,axis=0)
+        bin_acf_groups = np.split(acf_values,bins+1,axis=0)
+        
+        t_binavg = []
+        acf_binavg = []
+        #largest_binwidth = 0 # for testing
+
+        for i in range(len(bin_tgroups)):
+            t_binavg.append(np.mean(bin_tgroups[i]))
+            acf_binavg.append(np.mean(bin_acf_groups[i]))
+            # Code below is for testing
+#             if (bin_tgroups[i][-1]-bin_tgroups[i][0]) > 5*60:
+#                 print('bin larger than 5 minutes:',bin_tgroups[i][-1]-bin_tgroups[i][0])
+#             if (bin_tgroups[i][-1]-bin_tgroups[i][0] > largest_binwidth):
+#                 largest_binwidth = bin_tgroups[i][-1]-bin_tgroups[i][0]
+#                 largest_bini = i
+
+#         print(largest_binwidth/60,'mins')
+#         print(largest_bini)
+
+        # Add the dt=0 datapoint back in post-smoothing
+        smoothed_times = np.concatenate(([0],t_binavg))
+        smoothed_acf = np.concatenate(([zerobin],acf_binavg))
+        
+        return smoothed_times, smoothed_acf
+    
+    
+    def fit_acf(self,tvec,acf,dtmax=2*86400,functype='quadpeak'):
         '''Function to fit the given ACF to either a linear or exponential model. These simplistic models are only meant
         to approximate the early part of the ACF (<dtmax), and should not be used to model the full ACF.
         
         Parameters
         -----------
-        tvec: array of timescales dt at which the ACF is computed.
+        tvec: array of timescales dt at which the ACF is computed. In seconds.
         acf: value of the ACF at each dt in tvec.
         dtmax: Maximum (cutoff) dt of the ACF used for Kriging. Default 2x86400 seconds (48 hours).
-        functype: 'linear' or 'exponential', default 'linear'. Defines what model will be used to fit the ACF.
+        functype: 'linear', 'exponential', 'polyfit' (degree 4 polynomial), or 'quadpeak' (quadratic fit for dt>0, with added spike
+        of the height of ACF at dt=0). Default 'quadpeak'. Defines what model will be used to fit the ACF.
         '''
+        if functype == 'quadpeak':
+            # Quadratic for 0<dt<dtmax + dt=0 peak height
+            c = np.polyfit(x=tvec[(0<tvec)&(tvec<dtmax)]/3600,y=acf[(0<tvec)&(tvec<dtmax)],deg=2)
+            polyfunc = np.poly1d(c)
+
+            zeropeak_height = abs(acf[0] - acf[1])
+
+            def ACF_func(dt):
+                return polyfunc(dt) + delta_function(dt)
         
-        if functype == 'linear':
+        elif functype == 'linear':
             fit_func = linfunc
-        elif functype == 'exponenetial':
+            popt, pcov = scipy.optimize.curve_fit(f=fit_func,xdata=tvec[tvec<dtmax]/3600,ydata=acf[tvec<dtmax],p0=[-6e10/8,9.1e10])
+            ACF_func = ACF_fit(popt[0],popt[1],functype=functype)
+        elif functype == 'exponential':
             fit_func = expfunc
-        
-        popt, pcov = scipy.optimize.curve_fit(f=fit_func,xdata=tvec[tvec<dtmax]/3600,ydata=acf[tvec<dtmax],p0=[-6e10/8,9.1e10])
-        
-        ACF_func = ACF_fit(popt[0],popt[1],functype=functype)
+            popt, pcov = scipy.optimize.curve_fit(f=fit_func,xdata=tvec[tvec<dtmax]/3600,ydata=acf[tvec<dtmax],p0=[9.1e10,4])
+            ACF_func = ACF_fit(popt[0],popt[1],functype=functype)
+        elif functype == 'polyfit':
+            c = np.polyfit(x=tvec[tvec<dtmax]/3600,y=acf[tvec<dtmax],deg=4)
+            ACF_func = np.poly1d(c)
         
         return ACF_func
     
@@ -178,17 +251,13 @@ class Kriging:
         d = np.append(d_red,0)
         tarr = np.append(t_red,interp_time)
         C = np.zeros(shape=(len(d),len(d))) # initialize the covariance matrix
-        
+                    
         for i in range(len(d)):
-            for j in range(len(d)):
-                dtij = abs(tarr[i]-tarr[j]) # time separation between points
+            ti = tarr[i]
+            dtij_arr = abs(ti-tarr) # full array of the time separation from ti
+            #if np.any(dtij_arr>dtmax): print('larger') # normally this should not happen since we've already truncated the array
 
-                if dtij > dtmax:
-                    # For now, we are only using the ACF up to dt = dtmax. If dtij > dtmax, set C_ij = 0 (uncorrelated).
-                    # Normally this should not happen in the reduced matrix format we set up above.
-                    C[i,j] = 0
-                else:
-                    C[i,j] = ACF_func(dt=dtij/3600) # ACF_func is defined for dt in hours
+            C[i,:] = ACF_func(dtij_arr/3600) # ACF_func is defined for dt in hours
          
         
         # Invert the covariance matrix
